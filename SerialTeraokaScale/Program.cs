3f90f8bb1c87d6873f8cce68016a83d33c08f800
Zaid Kajee <zaid26@gmail.com>
2020-12-23 15:58:46 +0200

    Initial commit

---
+++ SerialTeraokaScale/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO.Ports;
+
+
+namespace SerialTeraokaScale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort port = new SerialPort("COM"+args[0], 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            byte[] bytestosend = { 0x05 };
+
+            port.Write(bytestosend, 0, bytestosend.Length);
+            string text = port.ReadLine();
+            System.IO.File.WriteAllText(args[1], text);
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+paramer 1 port number and parmeter 2 filename
+
+
+example: SerialTeraokaScale.exe 3 weight.txt
